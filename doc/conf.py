c87ced922e0b0c676c2ba882c9a25ba3a61b3ca2
ruden <valera.ruden@gmail.com>
2021-09-21 21:57:34 +0300

    Add conf.py

---
+++ doc/conf.py
@@ -0,0 +1,12 @@
+project = 'Twig'
+copyright = '2021, OSCLab.com'
+author = 'Ruden'
+release = '3.0'
+extensions = [
+]
+language = 'ru'
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+html_theme = 'furo'
+html_static_path = ['_static']
+html_show_sphinx = False
+html_copy_source = False
